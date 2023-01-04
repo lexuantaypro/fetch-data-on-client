f891575d192a727c84c65f626457c5c5895a8840
lexuantaypro <75997656+lexuantaypro@users.noreply.github.com>
2023-01-04 17:17:04 +0700

    Initial commit

---
+++ components/PokemonsSearchResult.tsx
@@ -0,0 +1,13 @@
+const PokemonsSearchResult = ({ pokemons }: { pokemons: string[] }) => {
+  return pokemons.length > 0 ? (
+    <div className="search-grid">
+      {pokemons.map((pokemon) => (
+        <div className="pokemon-card">{pokemon}</div>
+      ))}
+    </div>
+  ) : (
+    <div className="search-message"> No pokemons found</div>
+  );
+};
+
+export default PokemonsSearchResult;
---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import "../styles.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
+  return (
+    // Provide the client to your App
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
